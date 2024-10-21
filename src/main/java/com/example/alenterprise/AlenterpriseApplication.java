94c9b626015aed6f62aa3f0728f2a067dad408fa
Prajwal <praj3009wal@gmail.com>
2024-10-21 17:09:23 +0530

    fullbackend

---
+++ src/main/java/com/example/alenterprise/AlenterpriseApplication.java
@@ -0,0 +1,13 @@
+package com.example.alenterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlenterpriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlenterpriseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/alenterprise/AlenterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alenterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlenterpriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
